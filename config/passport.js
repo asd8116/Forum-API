075bbaea5381245e7befdf609fbc82d9579b9b27
wanaka <asd8116@gmail.com>
2019-07-12 16:49:21 +0800

    Add authenticated and authenticatedAdmin

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,25 @@ const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
 const Restaurant = db.Restaurant
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = 'alphacamp'
+
+let strategy = new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
+  const user = await User.findByPk(jwt_payload.id, {
+    include: [{ model: Restaurant, as: 'FavoritedRestaurants' }, { model: Restaurant, as: 'LikedRestaurants' }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }]
+  })
+
+  if (!user) return next(null, false)
+  return next(null, user)
+})
+
+passport.use(strategy)
 
 passport.use(
   new LocalStrategy(
@@ -12,12 +31,11 @@ passport.use(
       passwordField: 'password',
       passReqToCallback: true
     },
-    (req, username, password, cb) => {
-      User.findOne({ where: { email: username } }).then(user => {
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
-        if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-        return cb(null, user)
-      })
+    async (req, username, password, cb) => {
+      const user = await User.findOne({ where: { email: username } })
+      if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
+      if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+      return cb(null, user)
     }
   )
 )
@@ -26,12 +44,12 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 
-passport.deserializeUser((id, cb) => {
-  User.findByPk(id, {
+passport.deserializeUser(async (id, cb) => {
+  const user = await User.findByPk(id, {
     include: [{ model: Restaurant, as: 'FavoritedRestaurants' }, { model: Restaurant, as: 'LikedRestaurants' }, { model: User, as: 'Followers' }, { model: User, as: 'Followings' }]
-  }).then(user => {
-    return cb(null, user)
   })
+
+  return cb(null, user)
 })
 
 module.exports = passport
--- routes/apis.js
+++ routes/apis.js
@@ -5,20 +5,33 @@ const categoryController = require('../controllers/api/categoryController')
 const userController = require('../controllers/api/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
+const passport = require('../config/passport')
 
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurants/create', adminController.createRestaurant)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-router.get('/admin/restaurants/:id/edit', adminController.editRestaurant)
-router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      return next()
+    }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
 
-router.get('/admin/categories', categoryController.getCategories)
-router.post('/admin/categories', categoryController.postCategory)
-router.get('/admin/categories/:id', categoryController.getCategories)
-router.put('/admin/categories/:id', categoryController.putCategory)
-router.delete('/admin/categories/:id', categoryController.deleteCategory)
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/create', authenticated, authenticatedAdmin, adminController.createRestaurant)
+router.get('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.post('/admin/restaurants', authenticated, authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
+router.get('/admin/restaurants/:id/edit', authenticated, authenticatedAdmin, adminController.editRestaurant)
+router.put('/admin/restaurants/:id', authenticated, authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
+
+router.get('/admin/categories', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticated, authenticatedAdmin, categoryController.postCategory)
+router.get('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.put('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.putCategory)
+router.delete('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.deleteCategory)
 
 // JWT signin
 router.post('/signin', userController.signIn)
