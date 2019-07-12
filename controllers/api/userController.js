a28700da65630520efe0121bb298b01dbb18dc34
wanaka <asd8116@gmail.com>
2019-07-12 12:49:07 +0800

    Install jsonwebtoken & passport-jwt, POST /api/signin

---
+++ controllers/api/userController.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt-nodejs')
+const db = require('../../models')
+const { User, Comment, Restaurant, Favorite, Like, Followship } = db
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+let userController = {
+  signIn: async (req, res) => {
+    // 檢查必要資料
+    if (!req.body.email || !req.body.password) {
+      return res.json({ status: 'error', message: "required fields didn't exist" })
+    }
+    // 檢查 user 是否存在與密碼是否正確
+    let userEmail = req.body.email
+    let password = req.body.password
+    const user = await User.findOne({ where: { email: userEmail } })
+    if (!user) return res.status(401).json({ status: 'error', message: 'no such user found' })
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ status: 'error', message: 'passwords did not match' })
+    }
+    let payload = { id: user.id }
+    let token = jwt.sign(payload, process.env.JWT_SECRET)
+    res.json({
+      status: 'success',
+      message: 'ok',
+      token: token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+      }
+    })
+  }
+}
+
+module.exports = userController
--- routes/apis.js
+++ routes/apis.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../controllers/api/adminController')
 const categoryController = require('../controllers/api/categoryController')
+const userController = require('../controllers/api/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
@@ -18,5 +19,8 @@ router.post('/admin/categories', categoryController.postCategory)
 router.get('/admin/categories/:id', categoryController.getCategories)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
+
+// JWT signin
+router.post('/signin', userController.signIn)
 
 module.exports = router
